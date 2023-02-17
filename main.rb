1841cd36083bacdf5c64b08f3757e75c6eab9369
Shawn <shawn.carter@xclio.us>
2023-02-17 05:26:06 +0000

    Added so many things Delete actions, added some input validation to prevent SQL injection. CSS style changes, and sortable tables. There is still a lot of junk in here, this was just a long cram session. Need to comment and clean up the code ASAP.

--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
+require 'rubygems'
 require 'sinatra'
 require 'mysql2'
 require 'sinatra/config_file'
@@ -6,10 +7,12 @@ require 'json'
 config_file 'config.yml'
 #establish connection
 @db_pass = settings.db_pass
-client = Mysql2::Client.new(:host => "localhost", :username => "sinatra", :password => @db_pass, :database => "choices")
+client = Mysql2::Client.new(:host => "localhost", :username => "sinatra", :password => @db_pass, :database => "foodpicker")
 
 get '/' do
-  erb :entryform
+  results = client.query("SELECT * FROM foodplaces")
+  @items = results.to_a
+  erb :table
 end
 
 get '/add' do
@@ -22,7 +25,26 @@ get '/items' do
 end
 
 post '/add' do
-  client.query("insert into foodplaces (name, type, address) VALUES ('#{params[:name]}', '#{params[:type]}', '#{params[:address]}')")
+  name = params[:name]
+  type = params[:type]
+  address = params[:address]
+    
+  # Validate the user input
+  if name.nil? || name.strip.empty?
+    return "Please enter a valid name, not a blank space"
+  end
+
+  if type.nil? || type.strip.empty?
+    return "Please enter a valid type, not a blank space"
+  end
+
+  # Escape special characters in the input
+  name = client.escape(params[:name])
+  type = client.escape(params[:type])
+  address = client.escape(params[:address])
+  
+  client.query("INSERT INTO foodplaces (name, type, address) VALUES ('#{name}', '#{type}', '#{address}')")
+  #client.query("insert into foodplaces (name, type, address) VALUES ('#{params[:name]}', '#{params[:type]}', '#{params[:address]}')")
   redirect '/items'
 end
 
@@ -37,6 +59,18 @@ post '/random' do
   content_type :json
   {success: true, selectedPlace: @selected_place}.to_json
   
+end
+
+post '/delete' do
+  id = params[:id]
+  begin
+    client.query("DELETE FROM foodplaces WHERE id = #{id}")
+    content_type :json
+    { message: 'Entry deleted successfully' }.to_json
+  rescue
+    status 500
+    { message: 'Error deleting entry' }.to_json
+  end
 end
 
 
